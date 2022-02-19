eec1cc5cd32dc830c66c5d7c1a18b545a13a9b24
5gazero <kayo9410@gmail.com>
2022-02-19 23:56:03 +0900

    [mod]: background setting

--- new2022year-client/src/App.js
+++ new2022year-client/src/App.js
@@ -1,19 +1,18 @@
+import styled from "styled-components";
+
 function App() {
-  return (
-    <div
-      style={{
-        // backgroundColor: "green",
-        width: "400px",
-        height: "800px",
-        backgroundImage:
-          "url('https://2022-01-newyear.s3.ap-northeast-2.amazonaws.com/bg_default.png')",
-        backgroundPosition: "center",
-        backgroundSize: "cover",
-      }}
-    >
-      덕담 하나 주면 안 잡아 먹지!
-    </div>
-  );
+  return <Container>{/* 덕담 하나 주면 안 잡아 먹지! */}</Container>;
 }
+
+const Container = styled.div`
+  background-color: green;
+  width: 100vw;
+  max-width: 450px;
+  height: 100vh;
+  background-image: url("https://2022-01-newyear.s3.ap-northeast-2.amazonaws.com/bg_default.png");
+  background-position: center;
+  background-size: cover;
+  margin: auto;
+`;
 
 export default App;
