c91f8acfba4dfae0ada19d005b812cd3a243931e
5gazero <kayo9410@gmail.com>
2022-02-25 23:10:45 +0900

    [mod]: layout

--- new2022year-client/src/App.js
+++ new2022year-client/src/App.js
@@ -1,7 +1,16 @@
 import styled from "styled-components";
 
 function App() {
-  return <Container>{/* 덕담 하나 주면 안 잡아 먹지! */}</Container>;
+  return (
+    <Container>
+      <div className='title'>
+        <h1>덕담 하나 주면 안 잡아 먹지!</h1>
+        <p>
+          {user}님이 받은 덕담: <span>{count}개</span>
+        </p>
+      </div>
+    </Container>
+  );
 }
 
 const Container = styled.div`
@@ -13,6 +22,28 @@ const Container = styled.div`
   background-position: center;
   background-size: cover;
   margin: auto;
+  .title {
+    background-color: green;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding-top: 5vh;
+    color: #3f3732
+    h1 {
+      font-size: 22px;
+      background-color: #fff;
+      padding: 8px 16px;
+      border-radius: 16px;
+      margin-bottom: 16px;
+      p {
+        font-size: 18px;
+        span {
+          font-size: 22px;
+        }
+      }
+    }
+  }
 `;
 
 export default App;
