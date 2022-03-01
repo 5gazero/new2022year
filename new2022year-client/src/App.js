ba6d5f7559a67e342077806832603972ca954b74
5gazero <kayo9410@gmail.com>
2022-03-01 22:38:53 +0900

    [mod] design 변경

--- new2022year-client/src/App.js
+++ new2022year-client/src/App.js
@@ -1,80 +1,103 @@
+import { useState } from "react";
 import styled from "styled-components";
 
 function App() {
+  const [clicked, setClicked] = useState(false);
+  const modalHandler = () => {
+    // console.log("clicked!");
+    setClicked(true);
+  };
+
   return (
     <Container>
       <div className='title'>
-        <h1>덕담 하나 주면 안 잡아 먹지!</h1>
-        <p>
-          가영님이 받은 덕담: <span>6개</span>
-        </p>
+        <h1>HAPPY 2022</h1>
+        <h2>새해 덕담을 주고받아보세요!</h2>
       </div>
-      <div class='box'>
-        <img
-          onClick={() => {
-            alert("새해 복 많이 받으세요");
-          }}
-          src='https://2022-01-newyear.s3.ap-northeast-2.amazonaws.com/object3.png'
-        />
+      <div className='boxContainer'>
+        <div className='box'>
+          복
+          {/* <img
+            onClick={modalHandler}
+            src='https://2022-01-newyear.s3.ap-northeast-2.amazonaws.com/object3.png'
+          /> */}
+        </div>
       </div>
-      <div className='btn'>바구니에 덕담 남기기</div>
+      <div className='btnContainer'>
+        <div className='btn'>시작하기</div>
+      </div>
     </Container>
   );
 }
 
 const Container = styled.div`
-  background-color: green;
+  background-color: #fed63e;
   width: 100vw;
   max-width: 450px;
   height: 100vh;
-  background-image: url("https://2022-01-newyear.s3.ap-northeast-2.amazonaws.com/bg_default.png");
+  /* background-image: url("https://2022-01-newyear.s3.ap-northeast-2.amazonaws.com/bg_default.png"); */
   background-position: center;
   background-size: cover;
   margin: auto;
+  display: flex;
+  flex-direction: column;
   .title {
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
     padding-top: 5vh;
-    color: #3f3732;
+    color: #cf452a;
   }
   h1 {
-    font-size: 22px;
-    background-color: #fff;
+    font-size: 45px;
+    font-weight: 600;
     padding: 8px 16px;
     border-radius: 16px;
     margin-bottom: 16px;
-    p {
-      font-size: 18px;
-      span {
-        font-size: 22px;
-      }
-    }
   }
-  .box {
-    img {
-      width: 70px;
-      position: absolute;
-      left: 50%;
-      bottom: 50%;
-    }
+  h2 {
+    font-size: 22px;
   }
-  .btn {
-    width: 300px;
-    height: 50px;
-    background-color: #3f3732;
-    color: #fff;
-    font-size: 18px;
-    border-radius: 10px;
-    cursor: pointer;
+  .boxContainer {
     display: flex;
-    flex-direction: column;
     justify-content: center;
     align-items: center;
-    position: fixed;
+    height: 70vh;
+  }
+  .box {
+    background-color: #ffe380;
+    width: 400px;
+    height: 400px;
+    border-radius: 200px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    box-shadow: 7px 7px 15px 4px rgba(0, 0, 0, 0.3);
+    font-size: 130px;
+    color: #cf452a;
+  }
+  .btnContainer {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100px;
+  }
+  .btn {
+    width: 400px;
+    height: 80px;
+    background-color: #fed63e;
+    border: 5px solid #cf452a;
+    color: #cf452a;
+    font-size: 30px;
+    border-radius: 40px;
+    cursor: pointer;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    /* position: fixed;
     bottom: 16px;
-    left: calc(50% - 150px);
+    left: calc(50% - 150px); */
   }
 `;
 
--- new2022year-client/src/styles/globalStyle.js
+++ new2022year-client/src/styles/globalStyle.js
@@ -4,14 +4,14 @@ import reset from "styled-reset";
 const GlobalStyle = createGlobalStyle`
 ${reset}
 @font-face {
-  font-family: 'Cafe24Ssurround';
-  src: url('https://2022-01-newyear.s3.ap-northeast-2.amazonaws.com/Cafe24Ssurround.woff') format('woff');
+  font-family: 'twaysky';
+  src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_tway@1.0/twaysky.woff') format('woff');
 }
 * {
   box-sizing: border-box;
 }
 body {
-  font-family: 'Cafe24Ssurround';
+  font-family: 'twaysky';
 }
 `;
 
